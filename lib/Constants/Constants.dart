6bb6bd6bdedebe88e7d83d4b499635574db6977d
aklavya20 <aklavya36@gmail.com>
2025-05-13 21:45:45 +0530

    fourth commit

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   final String AppwriteEndpoint =
-      "https://8080-appwrite-integrationfor-r0qkffgkvqn.ws-us118.gitpod.io/v1";
-  final String AppwriteProjectId = "67f26b71002a16a7623d";
+      "https://8080-aklavya20-integrationfo-nyegpb4hh8i.ws-us118.gitpod.io/v1";
+  final String AppwriteProjectId = "68236a41003b6bb7ed6f";
   final String VerificationURL =
       "https://3000-aklavya20-instachatback-dwerag1m726.ws-us118.gitpod.io/verify";
 }
